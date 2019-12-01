4747fced35f7e042674d20a1d7bb0a413674bbe9
bluhb <rogiervtol@gmail.com>
2019-12-01 21:13:44 +0100

    created python solution

---
+++ day1/Python/main.py
@@ -0,0 +1,31 @@
+import math
+
+def readFile():
+    F = open("input.txt", "r")
+    Data = F.readlines()
+    return Data
+
+def fuelNeeded(Weight):
+    Fuel = 0
+    Fuel = math.floor(((Weight/3)-2))
+    if Fuel < 0:
+        return 0
+    return Fuel
+
+def main():
+    Data = readFile()
+    TotalFuel = 0
+    AddFuel = -1
+    for Weight in Data:
+        AddFuel = -1
+        while not (AddFuel == 0):
+            AddFuel = fuelNeeded(int(Weight))
+            TotalFuel += AddFuel
+            Weight = AddFuel
+
+    print(TotalFuel)
+    return None
+
+if __name__ == "__main__":
+    main()
+
