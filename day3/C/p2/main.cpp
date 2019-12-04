36d7ddd248dd7e4d57af6c0696b40d344aaae82c
bluhb <rogiervtol@gmail.com>
2019-12-04 11:34:52 +0100

    Solved part 2 in c++

--- day3/C/p2/main.cpp
+++ day3/C/p2/main.cpp
@@ -15,11 +15,11 @@
 //x= 0 1 2 3
 
 const int Lines = 2;
-const int Length = 10000;
+const int Length = 1000;
 const std::string File = "input.txt";
 
 std::set<std::vector <int>> Coordinates1;
-std::vector<std::vector <int>> Coordinates;
+std::vector<std::vector<std::vector <int>>> Coordinates;
 std::set<std::vector <int>> Intersections;
 
 std::vector <int> Origin = {0,0};
@@ -49,8 +49,8 @@ void readFile(std::string FileName){ //Read in the data file
     else {std::cout << "No file found";}
 }
 
-void add(char Dir, int Distance, bool CheckIntersection){
-    Current = Coordinates.back();
+void add(char Dir, int Distance, int Iteration, int Step){
+    Current = Coordinates[Iteration].back();
     for (int i=1; i<abs(Distance)+1; i++){
         if ((Dir == 'R') | (Dir == 'L')){
             New = {Current[0], Current[1] + i * (Distance/abs(Distance))};
@@ -58,13 +58,14 @@ void add(char Dir, int Distance, bool CheckIntersection){
             New = {Current[0] + i * (Distance/abs(Distance)), Current[1]};
         }
 
-        if (CheckIntersection && (Coordinates1.count(New) > 0)){
+        if (Iteration > 0 && (Coordinates1.count(New) > 0)){
+            New.push_back(Coordinates[Iteration].size());
             Intersections.insert(New);
-            std::cout << "Intersections found at: " << New[0] << ":" << New[1] << std::endl;
-        }else if (!CheckIntersection){
+            std::cout << "Intersections found at: " << New[0] << ":" << New[1] << " At: " << New[2] << std::endl;
+        }else if (Iteration == 0){
             Coordinates1.insert(New);
         }
-        Coordinates.push_back(New);
+        Coordinates[Iteration].push_back(New);
     }
 }
 
@@ -79,20 +80,45 @@ int minDist(){
             MinDist = Dist;
         }
     }
-    std::cout << "Point: " << MinIntersect[0] << ":" << MinIntersect[1];
+    std::cout << "Point: " << MinIntersect[0] << ":" << MinIntersect[1] << std::endl;
     return (MinDist);
+}
+
+int minSteps(){
+    int MinSteps = -1;
+    
+    for (std::vector<int> Intersect : Intersections){
+        int Step1 = 0;
+        int Step2 = 0;
+        int Steps = 0;
+        Step1 = Intersect.back();
+        Intersect.pop_back();
+        auto it = std::find(Coordinates[0].begin(), Coordinates[0].end(), Intersect);
+        auto found = *it;
+        std::cout << found[0] << ":"<< found[1] << " - " << Intersect[0] << ":" << Intersect[1] << std::endl;
+        Step2 = it - Coordinates[0].begin();
+        Steps = Step1 + Step2;
+        std::cout << Step1 << " + " << Step2 << " : " << Steps << std::endl;
+
+        if ((Steps < MinSteps) | (MinSteps == -1)){
+            MinSteps = Steps;
+        }
+
+    }
+
+    return (MinSteps);
 }
 
 int main(){
     std::string Distances;
     int Distance;
-
     readFile(File);
-    std::cout << File;
     Coordinates1.insert(Origin);
+    
+    std::vector<std::vector<int>> Start = {{0,0}};
+    Coordinates.push_back(Start);
+    Coordinates.push_back(Start);
     for (int i = 0; i < Lines; i++){
-        Coordinates.clear();
-        Coordinates.push_back(Origin);
         std::cout << "Line: " << i << std::endl;
         for (int j = 0; j< Length; j++){
             std::string Temp = Data[i][j];
@@ -104,11 +130,14 @@ int main(){
                     Distances = (Temp.substr(1));
                     Distance = std::stoi(Distances);
                 }
-                add(Temp[0], Distance, i);
+                add(Temp[0], Distance, i, j);
             }
         }
     }
     int MinimumDist = minDist();
+    int MinimumSteps = minSteps();
     std::cout << std::endl << "Min distance: " << MinimumDist << std::endl;
+
+    std::cout << std::endl << "Min Steps: " << MinimumSteps << std::endl;
     return 0;
 }
